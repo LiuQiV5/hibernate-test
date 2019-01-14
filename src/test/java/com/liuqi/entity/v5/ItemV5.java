c4827d92d96804a971825f21a7c2913874641bb6
LiuQiV5 <1504885830@qq.com>
2019-01-14 20:58:35 +0800

    映射枚举

--- src/test/java/com/liuqi/entity/v5/ItemV5.java
+++ src/test/java/com/liuqi/entity/v5/ItemV5.java
@@ -1,5 +1,6 @@
 package com.liuqi.entity.v5;
 
+import com.sun.istack.internal.NotNull;
 import org.hibernate.annotations.ColumnTransformer;
 
 import javax.persistence.*;
@@ -15,6 +16,10 @@ public class ItemV5 {
     @Column(name = "WEIGHT_G")
     @ColumnTransformer(read = "WEIGHT_G / 1000",write = "? * 1000")
     protected double weight_kg;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    protected AuctionType auctionType = AuctionType.HIGHEST_BID;
 
     public void setWeight_kg(double weight_kg) {
         this.weight_kg = weight_kg;
